29dd5353f4d8d0c166954dd3d0ad2b92a036bc32
fenil viroja <fenilviroja497@gmail.com>
2026-08-13 01:34:02 +0530

    added paragraph

---
+++ ch2(27).c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 66;
+    // int b=11;
+
+    // printf("Enter the value of a is :");
+    // scanf("%d",&a);
+
+    printf("The value of is : %d", a % 11);
+
+    return 0;
+}
+
+
+/*output
+The value of is : 0
+*/
---
+++ ch2(28).c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+
+    printf(" \n The value of a is :");
+    scanf("%d", &a);
+
+    printf(" \n %d", a % 5);
+
+    printf(" \n The value of b is :");
+    scanf("%d", &b);
+
+    printf(" \n %d", b % 10);
+
+    return 0;
+}
+
+/*
+output
+ The value of a is :10
+ 
+ 0 
+ The value of b is :20
+ 
+ 0
+*/
