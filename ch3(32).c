3b087a2cb7e4d2c941e02bf3479bda8aad3d7f35
fenil viroja <fenilviroja497@gmail.com>
2026-08-26 15:39:36 +0530

    added paragraph

---
+++ ch3(32).c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    float celsius, fahrenheit;
+
+    printf("Enter temperature in Celsius: ");
+    scanf("%f", &celsius);
+
+    fahrenheit = (celsius * 9 / 5) + 32;
+
+    printf("Temperature in Fahrenheit = %.2f", fahrenheit);
+    return 0;
+}
+
+
+/*
+output
+Enter temperature in Celsius: 10*
+Temperature in Fahrenheit = 50.00
+*/
