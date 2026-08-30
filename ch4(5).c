fdd480ab4c5332f8eba1531e3149ca8f026449eb
fenil viroja <fenilviroja497@gmail.com>
2026-08-31 00:33:57 +0530

    added paragraph

---
+++ ch4(5).c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    int i=5;
+    printf("\n The value of i is :%d",i);
+    i=i+5;
+    printf("\n The value of i is :%d",i);
+    i++;
+    printf("\n The value of i is :%d",i);
+    printf("\n The value of i is :%d",i++);
+    printf("\n The value of i is :%d",++i);
+
+    return 0;
+}
