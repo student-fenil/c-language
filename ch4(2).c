5e0f4ab1c5fa6c60c3fe68ef2bd10f6851267129
fenil viroja <fenilviroja497@gmail.com>
2026-08-30 12:05:03 +0530

    added paragraph

---
+++ ch4(2).c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int main()
+{
+    int i=0;
+
+    while (i<10)
+    {
+        printf("\n The value of i is : %d ",i);
+        i=i+1;
+    }
+    
+
+    return 0;
+}
+
+
+/*
+output
+ The value of i is : 0 
+ The value of i is : 1 
+ The value of i is : 2 
+ The value of i is : 3 
+ The value of i is : 4 
+ The value of i is : 5 
+ The value of i is : 6 
+ The value of i is : 7 
+ The value of i is : 8 
+ The value of i is : 9 
+*/
