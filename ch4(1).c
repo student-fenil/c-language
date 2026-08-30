ce08fce85ccaf115e8e1b2f6916a60ed5e41344f
fenil viroja <fenilviroja497@gmail.com>
2026-08-30 12:04:13 +0530

    added paragraph

---
+++ ch4(1).c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main()
+{
+    int i=0;
+
+    while (i<5)
+    {
+        printf("\n Happy birthday");
+        i=i+1;
+    }
+    
+
+    return 0;
+}
+
+
+/*
+output
+ Happy birthday
+ Happy birthday
+ Happy birthday
+ Happy birthday
+ Happy birthday
+ */
