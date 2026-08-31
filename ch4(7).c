54b408ec7c9244ff090f50a4ae01ac2ce2efeeab
fenil viroja <fenilviroja497@gmail.com>
2026-09-01 00:19:15 +0530

    added paragraph

--- ch4(7).c
+++ ch4(7).c
@@ -3,20 +3,31 @@
 int main()
 {
     int i=0;
+    int n;
+    printf("Enter any number of n :");
+    scanf("%d",&n);
+
     do
     {
         printf("\n The value of i is :%d",i);
         i++;
-    } while (i<4);
+    } while (i<=n); 
     
+
 
     return 0;
 }
 
+
+
 /*
 output
+Enter any number of n :
+4
+
  The value of i is :0
  The value of i is :1
  The value of i is :2
  The value of i is :3
+ The value of i is :4
 */
