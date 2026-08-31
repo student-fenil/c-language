296aed839b0fd1bae5981fe8933ce2a375c6bb6d
fenil viroja <fenilviroja497@gmail.com>
2026-09-01 00:20:11 +0530

    added paragraph

---
+++ ch4(8).c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main()
+{
+    int n = 6;
+    for (int i = 0; i < n; i++)
+    {
+        printf("The value of i is :%d \n",n);
+    }
+
+    return 0;
+    
+}
+
+
+
+/*
+output
+The value of i is :6 
+The value of i is :6 
+The value of i is :6 
+The value of i is :6 
+The value of i is :6 
+The value of i is :6 
+*/
