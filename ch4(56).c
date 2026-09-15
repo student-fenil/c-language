88b687a999e4998f2a2c580aec826234c7d48dbb
fenil viroja <fenilviroja497@gmail.com>
2026-09-15 11:53:26 +0530

    added paragraph

---
+++ ch4(56).c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("Enter any number of n :");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= 10; i++)
+    {
+        printf("%d X %d = %d\n", n, i, n * i);
+    }
+
+    return 0;
+}
+
+
+
+/*
+output
+Enter any number of n :5
+5 X 1 = 5
+5 X 2 = 10
+5 X 3 = 15
+5 X 4 = 20
+5 X 5 = 25
+5 X 6 = 30
+5 X 7 = 35
+5 X 8 = 40
+5 X 9 = 45
+5 X 10 = 50
+*/
